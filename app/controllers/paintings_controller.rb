5688006b3773f3548b3dda5392169295ca10f589
Aymeric <aymeric.francoise@gmail.com>
2018-01-30 14:42:31 +0100

    Correction bug affichage nombre tableaux notés

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -11,7 +11,7 @@ class PaintingsController < ApplicationController
   # home action
   def create
     @painting = Painting.new(painting_params)
-    @painting.users = session[:user_id]
+    @painting.users_id = session[:user_id]
     if @painting.save
       redirect_to '/generate'
     else
