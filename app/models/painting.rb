a86422cfe1f8784f8fcf6e9db86f9222f3fd29e9
Aymeric <aymeric.francoise@gmail.com>
2018-01-30 14:40:29 +0100

    Correction bug affichage nombre tableaux notés

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,4 +1,3 @@
 class Painting < ActiveRecord::Base
-  validates :mark, presence: true
   belongs_to :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130121404) do
+ActiveRecord::Schema.define(version: 20180130133822) do
 
   create_table "paintings", force: :cascade do |t|
     t.integer  "mark"
